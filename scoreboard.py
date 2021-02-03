9a989ed7dd5bbd7c5742f9ae7388cc47632b34ed
Enrique Gongora <EGTech24@gmail.com>
2021-02-02 16:21:41 -0800

    Implemented a high score functionality

--- scoreboard.py
+++ scoreboard.py
@@ -7,18 +7,21 @@ class Scoreboard(Turtle):
         self.color("white")
         self.penup()
         self.ht()
+        self.high_score = 0
         self.goto(0, 270)
         self.score = 0
         self.update_scoreboard()
 
     def increase_score(self):
         self.score += 1
-        self.clear()
         self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.write(f"Score: {self.score}", False, font=("Arial", 16, "normal"))
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.high_score}", False, font=("Arial", 16, "normal"))
 
-    def game_over(self):
-        self.goto(0,0)
-        self.write("GAME OVER!", align="Center", font=("Arial", 16, "normal"))
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.score = 0
+        self.update_scoreboard()
